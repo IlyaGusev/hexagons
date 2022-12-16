ea6303c65af6abf477f0fa51e79d677cd2f7f0da
Ilya Gusev <phoenixilya@gmail.com>
2022-12-16 13:19:05 +0000

    fix

--- hexagons/util.py
+++ hexagons/util.py
@@ -51,10 +51,10 @@ def fix_tokenizer(tokenizer):
     if tokenizer.sep_token_id in (None, tokenizer.vocab_size) and "bos_token" in special_tokens:
         special_tokens["sep_token"] = special_tokens["bos_token"]
 
-    if "pad_token" not in special_tokens or not special_tokens["pad_token"]:
+    if tokenizer.pad_token_id in (None, tokenizer.vocab_size) and "pad_token" not in special_tokens:
         special_tokens["pad_token"] = "<|pad|>"
 
-    if "sep_token" not in special_tokens or not pecial_tokens["sep_token"]:
+    if tokenizer.sep_token_id in (None, tokenizer.vocab_size) and "sep_token" not in special_tokens:
         special_tokens["sep_token"] = "<|sep|>"
 
     tokenizer.add_special_tokens(special_tokens)
